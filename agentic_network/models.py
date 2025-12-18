aa8c713fb540439b86eca31d2eb84915d25e9907
eyuil <43527367+eyu-il@users.noreply.github.com>
2025-12-18 00:03:18 -0800

    fixed models
    previous code had another class "Level" that was possibly created incorrectly (due to the dictionary being created as a class instead of a global dictionary), causing Exceptions to be raised.
    
    model.py now should work as intended
    admin.py now should work as intended

--- agentic_network/models.py
+++ agentic_network/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-class Level(models.Choices):
-    MIDDLE_SCHOOL = 'MIDDLE_SCHOOL'
-    HIGHSCHOOL = 'HIGHSCHOOL'
-    COMMUNITY_COLLEGE = 'COMMUNITY_COLLEGE'
-
 # Create your models here.
 class User(models.Model):
+    ACADEMIC_LEVELS = {
+        "MS" : "Middle School",
+        "HS" : "High School",
+        "CC" : "Community College",
+    }
     username = models.CharField(max_length=255)
-    password = models.CharFieldField(max_length=255)
+    password = models.CharField(max_length=255)
     application_url = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    level = models.CharField(max_length=255, choices=Level.choices)
+    level = models.CharField(max_length=255, choices=ACADEMIC_LEVELS)
     is_active = models.BooleanField(default=True)
